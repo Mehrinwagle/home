003b168a26ca058adfd13debc9fea5c852bb298a
MEHRIN WAGLE <mehrinwagle@gmail.com>
2025-08-25 00:39:19 +0530

    students marks

--- student.py
+++ student.py
@@ -3,19 +3,23 @@ class Student:
         self.name=name
         self.roll_no=roll_no
         self.marks=marks
+    def display(self):
+        print("Here is your output:")
     def details(self):
-        print("Name",self.name)
-        print("Roll No:", self.roll_no)
-        print("Marks:", self.marks)
+        print("Name :",self.name)
+        print("Roll No :", self.roll_no)
+        print("Marks :", self.marks)
+        if marks >= 40:
+            print("Result : Pass")
+        else:
+            print("Result : Fail")
 
 
 name=input("Enter Student Name: ")
 roll_no = int(input("Enter Roll Number: "))
 marks = int(input("Enter Marks: "))
-if marks >= 40:
-    print("Result: Pass")
-else:
-    print("Result: Fail")
+
 t=Student(name,roll_no,marks)
+t.display()
 t.details()
 
